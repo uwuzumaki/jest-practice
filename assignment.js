337fa9a67e2b746b2f5bd53f723825bf47b5e2cb
Kris Wong <krischwong@gmail.com>
2023-10-25 23:23:20 -0400

    Added reverseString function and calculator object and passed all tests.

--- assignment.js
+++ assignment.js
@@ -1,5 +1,18 @@
-const capitalize = (word) => {
-  return word[0].toUpperCase() + word.slice(1);
+const capitalize = (string) => {
+  return string[0].toUpperCase() + string.slice(1);
 };
 
-export { capitalize };
+const reverseString = (string) => {
+  return string.split("").reverse().join("");
+};
+
+const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  divide: (a, b) => a / b,
+  multiply: (a, b) => a * b,
+};
+
+const caesarCipher = (string, shift) => {};
+
+export { capitalize, reverseString, calculator };
--- assignment.test.js
+++ assignment.test.js
@@ -1,5 +1,24 @@
-import { capitalize } from "./assignment";
+import { calculator, capitalize, reverseString } from "./assignment";
 
 it("Capitalize the first character of a string", () => {
   expect(capitalize("button")).toMatch("Button");
+});
+
+it("Reverses a given string", () => {
+  expect(reverseString("button")).toMatch("nottub");
+});
+
+describe("Calculator", () => {
+  it("Addition", () => {
+    expect(calculator.add(1, 1)).toEqual(2);
+  });
+  it("Subtraction", () => {
+    expect(calculator.subtract(2, 1)).toEqual(1);
+  });
+  it("Divide", () => {
+    expect(calculator.divide(4, 2)).toEqual(2);
+  });
+  it("Multiply", () => {
+    expect(calculator.multiply(3, 3)).toEqual(9);
+  });
 });
